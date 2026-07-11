b6644e7b860113fac92506403b2555ee0d0ad9eb
Liv <oliviawurm01@gmail.com>
2026-07-11 23:47:12 +0200

    change Fonts TopMenu

--- src/lib/components/TopMenu.svelte
+++ src/lib/components/TopMenu.svelte
@@ -1,13 +1,13 @@
-<div class="flex justify-center items-center p-1 m-2 mt-10 mb-10 font-[Pacifico] text-3xl text-gray-300 text-shadow-black text-shadow-md">
+<div class="flex justify-center items-center p-1 m-2 mt-10 mb-[10vh] font-[Birthstone+Bounce] text-3xl text-gray-300 text-shadow-black text-shadow-md">
     <nav>
-        <a style="margin-right: 2vw;" href="/">Home</a>
-        <a href="/Portfolio">Portfolio</a>
+        <a style="margin-right: 2vw; font-[M+PLUS+U]" href="/">Home</a>
+        <a style="font-[M+PLUS+U]" href="/Portfolio">Portfolio</a>
     </nav>
-    <div class="text-center font-[Pacifico] text-6xl ml-8 mr-8">
-        <h1>Olivia Lua Photography</h1>
+    <div class="text-center text-6xl ml-[3vw] mr-[3vw]">
+        <h1 class="font-custom">Olivia Lua Photography</h1>
     </div>
     <nav>
-        <a href="/About">About</a>
-        <a style="margin-left: 2vw;" href="/Contact">Contact</a>
+        <a style="font-[M+PLUS+U]" href="/About">About</a>
+        <a style="margin-left: 2vw; font-[M+PLUS+U]" href="/Contact">Contact</a>
     </nav>
 </div>
