3330dfc88bb6c11ebb58f2bcda9a17713ca7c60c
Liv <oliviawurm01@gmail.com>
2026-07-24 14:59:50 +0200

    add horizontal lines to bottom menu

--- src/lib/components/BottomMenu.svelte
+++ src/lib/components/BottomMenu.svelte
@@ -9,15 +9,34 @@
 </script>
 
 <div class="flex justify-center items-center w-full">
-    <div class="relative flex bg-[#1E1E1E] w-full h-[40vh] mb-0">
-        <nav>
-            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md ml-[30vw] mt-15" style="{currentPage === "home" ? "text-decoration: underline;" : ""}" href="/">Home</a>
-            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md ml-[30vw] mt-8" style="{currentPage === "portfolio" ? "text-decoration: underline;" : ""}" href="/Portfolio">Portfolio</a>
-            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md ml-[30vw] mt-8" style="{currentPage === "about" ? "text-decoration: underline;" : ""}" href="/About">About</a>
-            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md ml-[30vw] mt-8" style="{currentPage === "contact" ? "text-decoration: underline;" : ""}" href="/Contact">Contact</a>    
-            <button type="button" class="absolute top-15 right-[30vw] cursor-pointer" onclick={gotoTop}>
-                <ArrowUp class="flex fill-gray-300 drop-shadow-md drop-shadow-black h-6 w-6 "/>
-            </button>
+    <div class="relative flex justify-center bg-[#1E1E1E] w-full h-[45vh] mb-0">
+        <hr class="absolute h-[1px] w-[70%] my-10 mx-auto bg-gray-300 border-0">
+        <nav class="absolute left-150 top-20 drop-shadow-md drop-shadow-black">
+            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md" 
+                style="{currentPage === "home" ? "text-decoration: underline;" : ""}" 
+                href="/">
+                Home
+            </a>
+            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md mt-8" 
+                style="{currentPage === "portfolio" ? "text-decoration: underline;" : ""}" 
+                href="/Portfolio">
+                Portfolio
+            </a>
+            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md mt-8" 
+                style="{currentPage === "about" ? "text-decoration: underline;" : ""}" 
+                href="/About">
+            About
+        </a>
+            <a class="flex font-[M+PLUS+U] text-gray-300 drop-shadow-md drop-shadow-black text-md mt-8" 
+                style="{currentPage === "contact" ? "text-decoration: underline;" : ""}" 
+                href="/Contact">
+                Contact
+            </a>    
         </nav>
+
+        <button type="button" class="absolute top-20 right-150 cursor-pointer" onclick={gotoTop}>
+            <ArrowUp class="flex fill-gray-300 drop-shadow-md drop-shadow-black h-6 w-6 "/>
+        </button>
+        <hr class="absolute h-[1px] w-[70%] my-80 mx-auto bg-gray-300 border-0 drop-shadow-md drop-shadow-black">
     </div>
 </div>
